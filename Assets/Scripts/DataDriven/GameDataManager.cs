35ee11670076e6bac3a8a1369ed2846b8bc138f7
SonghaCH <cwithus0002@gmail.com>
2026-05-12 23:43:41 +0900

    [DataDriven] 데이터 드리븐 요소 스크립트 학습 20260512
    json 데이터 드리븐을 위한 GameData,GameUtil,GameDataManager 스크립트를 타이핑하며 구조를 이해하고, 데이터 드리븐을 위한 사전 작업을 준비하였다. 사용만 할때보다 구조를 이해하며, 필요한 데이터만 골라서 할 수 있는 메리트를 느꼈으며, 앞으로의 작업이 기대 된다.

---
+++ Assets/Scripts/DataDriven/GameDataManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class GameDataManager : MonoBehaviour
+{
+    public static GameDataManager Instance { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+
+        // +++ C# 콘솔때와 다르게 이제 Main()함수가 아닌
+        // 모노의 메서드에서 호출될 수 있으므로, 데이터 매니저가 활성화되면 바로 모든 데이터를 한번 받아오자
+        // 이처리는 원하는 시점이 있다면 이전해도 된다
+        GameUtil.LoadFullData();
+    }
+
+    // --- JsonUtility의 한계를 극복하기 위한 Wrapper 클래스 ---
+    [Serializable]
+
+    private class SerializationWrapper<T>
+    {
+        public List<T> items; // JSON 파일의 루트 키 이름이 "items"여야 함
+    }
+
+    //public Dictionary<string, CharacterData> CharacterDataList { get; private set; } = new Dictionary<string, CharacterData>();
+
+    private Dictionary<string, T> LoadData<T>(string jsonPath) where T : GameDataBase
+    {
+        if(!File.Exists(jsonPath))
+        {
+            Debug.LogError($"[Error] 파일을 찾을 수 없습니다: {jsonPath}");
+            return new Dictionary<string, T>();
+        }
+
+        try
+        {
+            string jsonString = File.ReadAllText(jsonPath);
+
+            // JsonUtility는 List<T>를 직접 못 가져오므로 Wrapper를 사용합니다.
+            // 만약 JSON이 배열 형태([ {...}, {...} ])라면 아래 방식이 필요합니다.
+            // 만약 JSON 구조가 { "items": [...] } 형태가 아니라면 
+            // jsonString을 수정하여 강제로 감싸는 트릭을 써야 합니다.
+            string wrappedJson = "{\"items\":" + jsonString + "}";
+            SerializationWrapper<T> wrapper = JsonUtility.FromJson<SerializationWrapper<T>>(wrappedJson);
+
+            if (wrapper != null && wrapper.items != null)
+            {
+                Debug.Log($"{typeof(T).Name} 데이터를 {wrapper.items.Count}개 로드했습니다.");
+                return wrapper.items.ToDictionary(item => item.Id);
+            }
+
+
+
+        }
+
+        catch (Exception ex)
+        {
+            Debug.LogError($"[{typeof(T).Name} JSON 로드 오류] {ex.Message}");
+        }
+
+        return new Dictionary<string, T>();
+
+
+    }
+
+    //public void LoadCharacterData(string jsonPath)
+    //{
+    //    CharacterDataList = LoadData<CharacterData>(jsonPath);
+    //}
+
+
+    // [아래는 사용을 위한 부분들을 메서드 정의] =========================================================================================
+    // Get과 Find이름을 꼭 구별 하자!
+
+
+    //public CharacterData GetCharacterData(string id)
+    //{
+    //    if (CharacterDataList == null || string.IsNullOrEmpty(id)) return null;
+
+    //    return CharacterDataList.TryGetValue(id, out var item) ? item : null;
+    //}
+}
---
+++ Assets/Scripts/DataDriven/GameUtil.cs
@@ -0,0 +1,84 @@
+﻿using Cysharp.Threading.Tasks;
+using System.IO;
+using UnityEngine;
+
+public class GameUtil
+{
+    public static void LoadFullData()
+    {
+        //GameDataManager.Instance.LoadCharacterData(GetFullDataPath("Character"));
+
+    }
+
+    public static string GetFullDataPath(string dataTableName)
+    {
+        if (string.IsNullOrEmpty(dataTableName))
+        {
+            Debug.Log("테이블 이름이 올바르지 않습니다!");
+            return string.Empty;
+        }
+
+        // string path = @"C:예시-여러분의 상위 폴더\ComputerBasics_DaniTech_Work\JsonConverter\JsonOutput\Character.json";
+        // 제이슨 컨버터를 한번 실행해서 JsonCoutput에 Character.json을 미리 만들어뒀는지도 꼭 확인해주세요!
+        // 상대경로 ../../ 추후 툴 이름이나 경로가 변경되면 여길 확인하세요!
+        string relativePath = $"JsonConverter/JsonOutput/{dataTableName}.json";
+        string fullPath = Path.GetFullPath(relativePath);
+        return fullPath;
+    }
+
+    public static int CalcCharacterFinalDamage(int curCharacterLevel, int levelPerDamage, bool isCritical)
+    {
+        int damagePerLevel = (curCharacterLevel + levelPerDamage);
+        int finalDamage = isCritical ? (damagePerLevel * 2) : damagePerLevel;
+        return finalDamage;
+    }
+    //public static Sprite LoadSpriteCanBeNull(string spriteName)
+    //{
+    //    // 1. Resources/ 경로에서 이름으로 스프라이트 로드
+    //    // 예: spriteName이 "Sword"라면 Assets/Resources/2D/Sword.png를 찾음
+    //    // 이 2D같은 경로는 나중에 Sprite, Texture 등등 다양하게 바꿔도 무관합니다!
+    //    Sprite loadedSprite = Resources.Load<Sprite>($"{spriteName}");
+
+    //    if (loadedSprite != null)
+    //    {
+    //        return loadedSprite;
+    //    }
+
+    //    Debug.LogError($"에셋을 찾을 수 없습니다: {spriteName}");
+    //    return null;
+    //}
+
+    //public static async UniTask<Sprite> LoadAndSetSpriteImage(Image targetImage, string spritePath)
+    //{
+    //    Sprite sprite = await DaniTechResourceManager.Inst.LoadSprite(spritePath);
+    //    if (sprite != null)
+    //    {
+    //        targetImage.sprite = sprite;
+    //    }
+    //    return sprite;
+    //}
+
+    //public static async UniTaskVoid LoadAndPlayAudioClip(AudioSource audioSource, string audioPath, bool isLoop = false)
+    //{
+    //    AudioClip clip = await DaniTechResourceManager.Inst.LoadAsset<AudioClip>(audioPath);
+    //    if (clip == null)
+    //    {
+    //        Debug.LogError($"{audioPath}를 찾을 수 없습니다! 어드레서블 설정이 되어 있는지 확인해주세요.");
+    //        return;
+    //    }
+
+    //    if (isLoop == true)
+    //    {
+    //        audioSource.clip = clip;
+    //        audioSource.loop = true;
+    //        audioSource.Play();
+    //    }
+    //    else
+    //    {
+    //        audioSource.PlayOneShot(clip);
+    //    }
+    //}
+
+
+
+}
