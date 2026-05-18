e74eb2d7955711477031cc650d7b8025d7b02405
SonghaCH <cwithus0002@gmail.com>
2026-05-18 17:07:06 +0900

    [UI] 종료 팝업 버튼 구현

--- Assets/Scripts/UI/QuitPopup.cs
+++ Assets/Scripts/UI/QuitPopup.cs
@@ -5,14 +5,12 @@ public class QuitPopup : DaniTechUIBase
     [SerializeField] private DaniTechUIButton Btn_QuitYes;
     [SerializeField] private DaniTechUIButton Btn_QuitNO;
 
-    private void Start()
+    private void OnEnable()
     {
         Btn_QuitYes.BindOnClickButtonEvent(Onclick_QuitYes);
-        Btn_QuitNO.BindOnClickButtonEvent(Onclick_QuitNO);
+        Btn_QuitNO.BindOnClickButtonEvent(Onclick_QuitNo);
 
     }
-
-
 
 
     public void Onclick_QuitYes()
@@ -21,8 +19,9 @@ public class QuitPopup : DaniTechUIBase
         Application.Quit();
     }
 
-    public void Onclick_QuitNO()
+    public void Onclick_QuitNo()
     {
         Debug.Log("되돌아가기");
+        DaniTechUIManager.Instance.ClosePopupUI(DaniTechUIType.QuitPopupUI);
     }
 }
