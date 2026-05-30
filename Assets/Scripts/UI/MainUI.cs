2dd9fddfaf31f3160b929962cd17ec8ad19e57fc
SonghaCH <cwithus0002@gmail.com>
2026-05-31 02:06:41 +0900

    [UI] 지도 UI 제작 및 타일맵 수정

--- Assets/Scripts/UI/MainUI.cs
+++ Assets/Scripts/UI/MainUI.cs
@@ -7,6 +7,8 @@ public class MainUI : DaniTechUIBase
     [SerializeField] private Text Text_Time;
 
     [SerializeField] private DaniTechUIButton Button_Option;
+    [SerializeField] private DaniTechUIButton Button_Map;
+
 
     [SerializeField] private Text Text_Health;
     [SerializeField] private Text Text_Int;
@@ -17,11 +19,17 @@ public class MainUI : DaniTechUIBase
     private void OnEnable()
     {
         Button_Option.BindOnClickButtonEvent(Onclick_Option);
+        Button_Map.BindOnClickButtonEvent(Onclick_Map);
     }
 
 
     private void Onclick_Option()
     {
         Debug.Log("설정 열기");
+    }
+
+    private void Onclick_Map()
+    {
+        Debug.Log("지도 열기");
     }
 }
